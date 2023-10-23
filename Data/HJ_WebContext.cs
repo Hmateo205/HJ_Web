fda44af367ac04e32429db8d30cca5563dd69bd4
laboratorio <laboratorio@UPOAULA10712>
2023-10-23 08:51:51 -0500

    Agregar archivos de proyecto.

---
+++ Data/HJ_WebContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HJ_Web.Models;
+
+namespace HJ_Web.Data
+{
+    public class HJ_WebContext : DbContext
+    {
+        public HJ_WebContext (DbContextOptions<HJ_WebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HJ_Web.Models.Burger> Burger { get; set; } = default!;
+    }
+}
